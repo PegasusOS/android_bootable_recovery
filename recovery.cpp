1592629d2c9c05ffb51e33db54103cd443fe4fa1
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-12 14:13:31 -0400

    recovery_ui: Tell the user they're actually formatting
    * TWRP has given user the sentiment that "Wiping data",
      and "Factory Reset" both imply the exclusion of
      files in `/data/media/0` (/sdcard), whereas both AOSP
      (and Lineage Recovery by proxy) format the partitions.
    
    Change-Id: I36c5a59552d7d538a05762ca262e42dc78a1deac

--- recovery.cpp
+++ recovery.cpp
@@ -178,8 +178,8 @@ bool ask_to_continue_unverified(Device* device) {
 }
 
 static bool ask_to_wipe_data(Device* device) {
-  std::vector<std::string> headers{ "Wipe all user data?", "  THIS CAN NOT BE UNDONE!" };
-  std::vector<std::string> items{ " Cancel", " Factory data reset" };
+  std::vector<std::string> headers{ "Format user data?", "This includes internal storage.", "THIS CANNOT BE UNDONE!" };
+  std::vector<std::string> items{ " Cancel", " Format data" };
 
   size_t chosen_item = device->GetUI()->ShowPromptWipeDataConfirmationMenu(
       headers, items,
@@ -457,7 +457,7 @@ static Device::BuiltinAction PromptAndWait(Device* device, InstallResult status)
       case Device::WIPE_CACHE: {
         save_current_log = true;
         std::function<bool()> confirm_func = [&device]() {
-          return yes_no(device, "Wipe cache?", "  THIS CAN NOT BE UNDONE!");
+          return yes_no(device, "Format cache?", "  THIS CAN NOT BE UNDONE!");
         };
         WipeCache(ui, ui->IsTextVisible() ? confirm_func : nullptr);
         if (!ui->IsTextVisible()) return Device::NO_ACTION;
